6f659720efe99d7f990be29bdb4a781de996b379
carlos <satizabal44@gmail.com>
2020-03-10 08:28:00 +0000

    FormRequest modificado

--- app/Http/Requests/PacientesRequest.php
+++ app/Http/Requests/PacientesRequest.php
@@ -34,7 +34,7 @@ class PacientesRequest extends FormRequest
 			'apellido_2' => 'required|max:20|min:2|alpha',
 			'telefono' => ['required','max:9','min:9', new Telefono],
 			'dni' => ['required','max:10','min:9','unique:pacientes', new Dni],
-			'fecha_nacimiento' => 'required|date|after:1/1/1900|before:4/3/2020',
+			'fecha_nacimiento' => 'required|date|after:1/1/1900|before:11/3/2020',
 			'pais' => 'required|max:50|min:2',
 			'ciudad' => 'required|max:50|min:2',
 			'email' => ['max:60', new Email],
